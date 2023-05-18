a6ae9848035365f8dd779a0748ec6d7b577b1ca0
Kendall Chen <kendallgit@gmail.com>
2023-05-18 14:43:36 -0700

    add azure library

---
+++ api/Api/Controllers/ChatController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.AI.OpenAI;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<String> GetResponse()
+        {
+            var openAiClient = new OpenAIClient(
+                new Uri("endPointHere"),
+                new AzureKeyCredential("keyHere")
+            );
+
+            var chatCompletionOptions = new ChatCompletionsOptions { };
+            chatCompletionOptions.Messages.Add(new ChatMessage(ChatRole.User, "Who is the tallest in NBA?"));
+
+            var chatCompletionsResponse = await openAiClient.GetChatCompletionsAsync(
+                    "modelNameHere",
+                    chatCompletionOptions
+                );
+
+            return chatCompletionsResponse.Value.Choices[0].Message.Content;
+        }
+    }
+}
