39809bd933338f6a247e9f2788e005f47c843cc3
Kendall Chen <Kendall.Chen@kp.org>
2023-06-07 11:26:58 -0700

    update header and footer

--- ui/src/app/footer/footer.component.ts
+++ ui/src/app/footer/footer.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  thisYear:number = new Date().getFullYear();
+
   constructor() { }
 
   ngOnInit(): void {
