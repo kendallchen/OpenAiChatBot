3fc108a90fd3fd3c31dd40c11b71bae535f186e2
kendallchen <kendallgit@gmail.com>
2023-07-23 09:44:05 -0700

    move chat history out of service

--- ui/src/app/chat/chat.component.ts
+++ ui/src/app/chat/chat.component.ts
@@ -1,33 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
-import { IChatMsg } from '../model/IChatMsg';
+import { IChatMsg, MessageType } from '../model/IChatMsg';
 import { ChatService } from '../service/chat.service';
 
 @Component({
-  selector: 'app-chat',
-  templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.scss']
+    selector: 'app-chat',
+    templateUrl: './chat.component.html',
+    styleUrls: ['./chat.component.scss']
 })
 export class ChatComponent implements OnInit {
 
-    chatHistory: IChatMsg[];
+    chatHistory: IChatMsg[] = [];
     frmQuery: FormGroup;
 
-    constructor(private chatSvc:ChatService,
-                private fb: FormBuilder) { }
+    constructor(private chatSvc: ChatService,
+        private fb: FormBuilder) { }
 
     ngOnInit(): void {
         this.frmQuery = this.fb.group({
             txtQuery: [null]
-        }); 
+        });
     }
 
-    submitQuery(){  
-        let query = this.frmQuery.get("txtQuery").value;
-        this.chatSvc.chat(query)
-            .subscribe(data=>
-                this.chatHistory = data
-            );
+    submitQuery() {
+        let queryMessage: IChatMsg = { type: MessageType.Send, 
+                                       message : this.frmQuery.get("txtQuery").value };
+        this.chatHistory.push(queryMessage);
+        this.chatSvc.chat(queryMessage)
+            .subscribe(data => {
+                if (data)
+                {
+                    this.chatHistory.push(data);
+                }
+            });
     }
 
 }
--- ui/src/app/service/chat.service.ts
+++ ui/src/app/service/chat.service.ts
@@ -11,27 +11,20 @@ export class ChatService {
 
     readonly url = this.appConfig.get('apiChat');
 
-    private chatHistory: IChatMsg[] = [];
-
-    private response = new BehaviorSubject<IChatMsg[] | null>(null);
+    private response = new BehaviorSubject<IChatMsg | null>(null);
     response$ = this.response.asObservable();
 
     constructor(private http:HttpClient,
                 private appConfig:AppConfigService){}
                 
-    chat(query): Observable<IChatMsg[]>{
-        //add to chat history
-        const sendMsg : IChatMsg = { type: MessageType.Send, message : query };
-        this.chatHistory.push(sendMsg);
-
+    chat(queryMessage: IChatMsg): Observable<IChatMsg>{
         //get response
-        let queryUrl = this.url + "?query=" + query;
+        let queryUrl = this.url + "?query=" + queryMessage.message;
         this.http.get<IChatMsg>(queryUrl).pipe()
             .subscribe(result=>{
                 if (result){
                     const receiveMsg : IChatMsg = { type: MessageType.Receive, message : result.message };
-                    this.chatHistory.push(receiveMsg);
-                    this.response.next(this.chatHistory)
+                    this.response.next(receiveMsg)
                 }                    
             }
         )
